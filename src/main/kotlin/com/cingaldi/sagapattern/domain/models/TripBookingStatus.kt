fa93a119177dc2e4a69cb0cfabe8fdeefb8773cc
carmine <carmine.ingaldi@glovoapp.com>
2021-05-09 17:35:12 +0200

    use compisite pk

--- src/main/kotlin/com/cingaldi/sagapattern/domain/models/TripBookingStatus.kt
+++ src/main/kotlin/com/cingaldi/sagapattern/domain/models/TripBookingStatus.kt
@@ -5,27 +5,31 @@ import com.cingaldi.commons.Saga
 import com.cingaldi.sagapattern.application.commands.BookFlightCmd
 import com.cingaldi.sagapattern.application.commands.BookHotelCmd
 import com.cingaldi.sagapattern.application.commands.ConfirmTripCmd
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.Version
+import java.io.Serializable
+import javax.persistence.*
 
 @Entity
+@IdClass(TripBookingId::class)
 class TripBookingStatus(): Saga() {
 
     //association data
     @Id
-    var tripId: String = ""
+    lateinit var flightCode: String
         private set
-    var flightCode: String = ""
+    @Id
+    lateinit var hotelCode: String
         private set
-    var hotelCode: String = ""
+    @Id
+    lateinit var tripId: String
         private set
+
 
     //this saga supports concurrent events :)
     @Version
     private var version: Long = 0
 
     //saga progress data
+
     private var flightBooked: Boolean = false
     private var hotelBooked: Boolean = false
 
@@ -74,4 +78,37 @@ class TripBookingStatus(): Saga() {
 
         //you know how to do it
     }
+}
+
+@Embeddable
+class TripBookingId (): Serializable {
+
+    lateinit var hotelCode: String
+        private set
+    lateinit var flightCode : String
+        private set
+    lateinit var tripId: String
+        private set
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as TripBookingId
+
+        if (hotelCode != other.hotelCode) return false
+        if (flightCode != other.flightCode) return false
+        if (tripId != other.tripId) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = hotelCode.hashCode()
+        result = 31 * result + flightCode.hashCode()
+        result = 31 * result + tripId.hashCode()
+        return result
+    }
+
+
 }
